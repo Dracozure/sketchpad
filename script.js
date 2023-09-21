735efd9b9820c90ca7c967dc1cf209a601505565
Quan <61860115+Dracozure@users.noreply.github.com>
2023-09-21 00:21:27 -0700

    Fix broken variable of populateGrid function
    When trying to get a property value of element from window., make
    sure that element is defined in the DOM, or else will not result in
    correct output

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function createGrid(rowCount, columnCount, elementID = null, elementClasses = 'null') {
+const body = document.querySelector('body');
+
+const myGrid = createGrid(16, 16);
+
+populateGrid(myGrid);
+
+myGrid.classList.add('grid-container');
+
+body.appendChild(myGrid);
+
+function createGrid(rowCount, columnCount, elementID = null, elementClasses = 'grid') {
     const gridContainer = document.createElement('div');
     const classArr = elementClasses.split(' ');
 
     gridContainer.style.display = 'grid';
-    gridContainer.style.setProperty('grid-template-columns'
-    , `repeat(${columnCount}, 1fr)`);
-    gridContainer.style.setProperty('grid-template-rows'
-    , `repeat(${rowCount}, 1fr)`);
+    gridContainer.style
+    .setProperty('grid-template-columns', `repeat(${columnCount}, 1fr)`);
+    gridContainer.style
+    .setProperty('grid-template-rows', `repeat(${rowCount}, 1fr)`);
 
     if (elementID !== null) {
         gridContainer.setAttribute('id', addID);
@@ -16,16 +26,19 @@ function createGrid(rowCount, columnCount, elementID = null, elementClasses = 'n
         classArr.map((className) => {
             gridContainer.classList.add(className);
         });
+    } else {
+        gridContainer.classList.add(elementClasses);
     }
 
     return gridContainer;
 }
 
-function populateGrid(grid, elementClasses = 'grid-element') {
-    const gridRowCount = window.getComputedStyle(grid).getPropertyValue('grid-template-rows').split(' ').length;
-    const gridColumnCount = window.getComputedStyle(grid).getPropertyValue('grid-template-columns').split(' ').length;
-    const gridArea = gridRowCount ** gridColumnCount;
-    const classArr = elementClasses.split(' ');
+function populateGrid(grid, gridElementClasses = 'grid-element') {
+    //getPropertyValue(...).length results in one less than the actual length of row/column
+    const gridColumnsCount = grid.style.getPropertyValue('grid-template-columns').length + 1;
+    const gridRowsCount = grid.style.getPropertyValue('grid-template-rows').length + 1;
+    const gridArea = gridRowsCount * gridColumnsCount;
+    const classArr = gridElementClasses.split(' ');
 
     for (let i = 0; i < gridArea; i++) {
         const gridElement = document.createElement('div');
@@ -34,6 +47,8 @@ function populateGrid(grid, elementClasses = 'grid-element') {
             classArr.map((className) => {
                 gridElement.classList.add(className);
             });
+        } else {
+            gridElement.classList.add(gridElementClasses);
         }
 
         grid.appendChild(gridElement);
