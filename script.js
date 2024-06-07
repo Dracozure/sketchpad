c2b5c6bec8a386b2471db51ff93ebf35a77b313f
Quan <61860115+Dracozure@users.noreply.github.com>
2024-06-06 23:03:35 -0700

    Add dynamic grid generation function
    When creating new elements, don't chain with
    other methods or else it will give error When
    being added as a child element.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let gridBox = createGrid();
+const createGrid = (size) => {
+    const gridArea = size ** 2;
+    const gridContainer = document.querySelector('.container');
+    const grid = document.createElement('div')
+    
+    grid.classList.add('grid')
+    grid.style.display = 'grid';
+
+    for (let i = 0; i < gridArea; i++) {
+        gridCell = document.createElement('div');
+        gridCell.classList.add('cell');
+
+        grid.appendChild(gridCell);
+    }
+
+    grid.style.setProperty('grid-template-columns', `repeat(${size}, 1fr)`);
+    grid.style.setProperty('grid-template-rows', `repeat(${size}, 1fr)`);
+
+    gridContainer.appendChild(grid);
+}
