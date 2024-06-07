4260c2b49c60960bd0309887aecd6a2e14983c0c
Quan <61860115+Dracozure@users.noreply.github.com>
2024-06-07 01:22:03 -0700

    Add grid coloring functionality
    The addEventListener method will take 2 arguments.
    The second argument involving the function cannot
    pass in the original element that called the
    event listener method

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 window.onload = createGrid(16);
 
-let editorMode = "pen";
+let editorMode = 'pen';
+
+let mouseDownStatus = 0;
+
+window.onmouseup = () => {
+    mouseDownStatus = 0;
+}
+
+window.onmousedown = () => {
+    mouseDownStatus = 1;
+}
+
+const gridCells = Array.from(document.querySelector('.grid.box').children);
+
+arrCellsAddEventListeners(gridCells);
+
+function toggleColor() {
+    this.style.backgroundColor = 'red';
+}
+
+function toggleColorDrag() {
+    if (mouseDownStatus) {
+        this.style.backgroundColor = 'red';
+    }
+}
+
+function arrCellsAddEventListeners(array) {
+    array.forEach((element) => {
+        element.addEventListener('dragstart', event => {
+            event.preventDefault();
+        })
+        element.addEventListener('mousedown', toggleColor);
+        element.addEventListener('mouseover', toggleColorDrag);
+    });
+}
 
 function createGrid(size) {
     const gridArea = size ** 2;
     const gridContainer = document.querySelector('.grid.container');
-    const grid = document.createElement('div')
+    const grid = document.createElement('div');
     
-    grid.classList.add('grid', 'box')
+    grid.classList.add('grid', 'box');
     grid.style.display = 'grid';
 
     for (let i = 0; i < gridArea; i++) {
